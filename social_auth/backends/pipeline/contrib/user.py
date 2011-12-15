c9cd1b879d2dde714716ad34996e006b730f0778
Andrew Vavilov <vapask@gmail.com>
2011-12-15 19:38:58 +0400

    new pipeline

---
+++ social_auth/backends/pipeline/contrib/user.py
@@ -0,0 +1,24 @@
+def create_user(backend, details, response, uid, username, user=None, *args,
+                **kwargs):
+    """Create user without email. Depends on get_username pipeline."""
+    if user:
+        return {'user': user}
+    if not username:
+        return None
+
+    warn_setting('SOCIAL_AUTH_CREATE_USERS', 'create_user')
+
+    if not getattr(settings, 'SOCIAL_AUTH_CREATE_USERS', True):
+        # Send signal for cases where tracking failed registering is useful.
+        socialauth_not_registered.send(sender=backend.__class__,
+                                       uid=uid,
+                                       response=response,
+                                       details=details)
+        return None
+
+    email = details.get('email')
+    return {
+        'user': User.objects.create_user(username=username, email=email),
+        'is_new': True
+    }
+
